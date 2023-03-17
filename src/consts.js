ead469b2cfde974218cf22b6561d5fe4256a8797
emyia2001 <1762855928@qq.com>
2023-03-17 16:06:12 +0800

    init

---
+++ src/consts.js
@@ -0,0 +1,5 @@
+export const SITE_TITLE = `Snark's Blog`;
+export const SITE_DESCRIPTION = 'Austin Site Description';
+export const SITE_EMAIL = '1762855928@qq.com'
+export const SITE_NAME = 'astro.yufengbiji.com';
+export const SITE_URL = "https://astro.yufengbiji.com";
