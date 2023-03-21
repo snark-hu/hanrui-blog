6050db804c809ab2e334fd77099b5bc49405d493
snark-hu <128483540+snark-hu@users.noreply.github.com>
2023-03-21 22:11:02 +0800

    Update consts.js

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-export const SITE_TITLE = `Snark's Blog`;
+export const SITE_TITLE = `Hanrui's Blog`;
 export const SITE_DESCRIPTION = 'Austin Site Description';
-export const SITE_EMAIL = 'snarkd@qq.com'
-export const SITE_NAME = 'snark-hu.netlify.app';
+export const SITE_EMAIL = '3343181948@qq.com'
+export const SITE_NAME = 'hanrui-blog.netlify.app';
 export const SITE_URL = "https://snark-hu.netlify.app";
